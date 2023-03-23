1f58f575c026be2409338398e39ed56576370ad2
Manik Hosen <manik.hosen@bizzntek.com>
2023-03-23 11:37:09 +0600

    feat: db connected

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,11 +1,17 @@
+require('dotenv').config({ path: '.env' });
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorsController } from 'src/controllers/authors.controller';
-import { AppController } from '../controllers/app.controller';
-import { AppService } from '../services/app.service';
+import { AppController } from 'src/controllers/app.controller';
+import { AppService } from 'src/services/app.service';
+import { AuthorSchema } from 'src/schemas/authors.schema';
+import { AuthorService } from 'src/services/authors.service';
+
+const mongo_uri = process.env.DATABASE_URL;
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(mongo_uri), MongooseModule.forFeature([{ name: 'Author', schema: AuthorSchema }])],
   controllers: [AppController, AuthorsController],
-  providers: [AppService]
+  providers: [AppService, AuthorService]
 })
 export class AppModule {}
