7aeb403f7cb461c4254720371dfff86295362b11
Manik Hosen <manik.hosen@bizzntek.com>
2023-03-23 14:35:34 +0600

    feat: book crud done

---
+++ src/controllers/books.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Book } from 'src/schemas/books.schema';
+import { BookService } from 'src/services/books.service';
+
+@Controller('books')
+export class BooksController {
+  constructor(private booksService: BookService) {}
+
+  @Get()
+  async findAll(): Promise<Book[]> {
+    return this.booksService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.booksService.findById(id);
+  }
+
+  @Post()
+  create(@Body() book: Book) {
+    return this.booksService.create(book);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() book: Book) {
+    return this.booksService.update(id, book);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.booksService.delete(id);
+  }
+}
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -6,12 +6,21 @@ import { AppController } from 'src/controllers/app.controller';
 import { AppService } from 'src/services/app.service';
 import { AuthorSchema } from 'src/schemas/authors.schema';
 import { AuthorService } from 'src/services/authors.service';
+import { BooksController } from 'src/controllers/books.controller';
+import { BookService } from 'src/services/books.service';
+import { BookSchema } from 'src/schemas/books.schema';
 
 const mongo_uri = process.env.DATABASE_URL;
 
 @Module({
-  imports: [MongooseModule.forRoot(mongo_uri), MongooseModule.forFeature([{ name: 'Author', schema: AuthorSchema }])],
-  controllers: [AppController, AuthorsController],
-  providers: [AppService, AuthorService]
+  imports: [
+    MongooseModule.forRoot(mongo_uri),
+    MongooseModule.forFeature([
+      { name: 'Author', schema: AuthorSchema },
+      { name: 'Book', schema: BookSchema }
+    ])
+  ],
+  controllers: [AppController, AuthorsController, BooksController],
+  providers: [AppService, AuthorService, BookService]
 })
 export class AppModule {}
